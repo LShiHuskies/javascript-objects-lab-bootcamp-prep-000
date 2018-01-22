09118a2aec77ee32b6b6dcdbb0a4bf1946eea94f
Louis Shi <louis.shi@uconn.edu>
2018-01-22 02:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   var copy = Object.assign({}, object)
   copy[key] = value
    return copy
@@ -15,4 +16,9 @@ return delete copy.key
 function destructivelyDeleteFromObjectByKey (object, key) {
    delete object[key]
    return object
+=======
+  var newObj = object
+  newObj[key] = value
+   return newObj
+>>>>>>> a913bf2e54e3df87c9805835bb11ad676a4140ff
 }
